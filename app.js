403e2160c0eacd76e7cf337598cf210842e89477
clarkgh <clarkhinchcliff@gmail.com>
2023-03-13 23:34:30 -0400

    remove image file, update gitignore.

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const ffmpeg = require('fluent-ffmpeg');
-const ffmpegPath = require('ffmpeg-static').path || ffmpeg.setFfmpegPath('./node_modules/ffmpeg-static/ffmpeg'); // Set the path to the ffmpeg binary
 const path = require('path');
+const ffmpegPath = require('ffmpeg-static').path || ffmpeg.setFfmpegPath('./node_modules/ffmpeg-static/ffmpeg'); // Set the path to the ffmpeg binary
 
-function processSlideshow(imageFilePath, slideDuration) {
+function processSlideshow() {
   return new Promise((resolve, reject) => {
     const outputFilePath = path.join(__dirname, 'slideshow.mp4');
+    const IMAGE_INPUTS = './images/image%2d.jpeg';
 
+    console.log('Here we a go.');
 
-    ffmpeg(imageFilePath)
-      .inputOptions(['-loop 1'])
+    ffmpeg()
+      .input(IMAGE_INPUTS)
       .output(outputFilePath)
       .videoCodec('libx264')
-      .outputFPS(1 / slideDuration)
+      .outputFPS(1)
+      .frames(5)
       .on('end', () => resolve(outputFilePath))
       .on('error', (err) => reject(err))
       .run();
   });
 }
 
-const imageFilePath = path.join(__dirname, 'images/tuhtle.jpeg');
-const slideDuration = 5;
-
-processSlideshow(imageFilePath, slideDuration)
+processSlideshow()
   .then((outputFilePath) => console.log(`Slideshow processed successfully: ${outputFilePath}`))
   .catch((err) => console.error(`Error processing slideshow: ${err}`));
